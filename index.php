69178ee0d4eab77c9fafeef4bf9fdd12064de0ba
AntonioDanielMuniz <143140455+AntonioDanielMuniz@users.noreply.github.com>
2023-08-24 17:11:30 -0600

    index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP SERVER</title>
+</head>
+<body>
+    <div>Hola server</div>
+</body>
+</html>
